25cd110ab83e7c94bb84385246564bc350d520ac
Angrbeast52 <jonathanmorillocm@hotmail.com>
2021-05-14 12:43:58 -0500

    second commit

--- src/cryptocurrency/cryptocurrency.module.ts
+++ src/cryptocurrency/cryptocurrency.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CryptopcurrencyService } from './cryptocurrency.service';
 import { CryptocurrencyController } from './cryptocurrency.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cryptopcurrency,CryptopcurrencySchema } from './schema/Cryptocurrency.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Cryptopcurrency.name, schema: CryptopcurrencySchema },
+    ]),
+  ],
   providers: [CryptopcurrencyService],
-  controllers: [CryptocurrencyController]
+  controllers: [CryptocurrencyController],
 })
 export class CryptocurrencyModule {}
